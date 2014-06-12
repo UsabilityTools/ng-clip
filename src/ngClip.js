1df0c5af8603199aec504ce7e0a4c464e87f0086
Eryk Napierała <eryk.piast@gmail.com>
2014-06-12 12:34:41 +0200

    wrap in AMD define conditionally

--- src/ngClip.js
+++ src/ngClip.js
@@ -1,59 +1,64 @@
-/*jslint node: true */
-/*global ZeroClipboard */
-'use strict';
-
-angular.module('ngClipboard', []).
-  provider('ngClip', function() {
-    var self = this;
-    this.path = '//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/1.3.5/ZeroClipboard.swf';
-    return {
-      setPath: function(newPath) {
-       self.path = newPath;
-      },
-      $get: function() {
-        return {
-          path: self.path
-        };
-      }
-    };
-  }).
-  run(['ngClip', function(ngClip) {
-    ZeroClipboard.config({
-      moviePath: ngClip.path,
-      trustedDomains: ["*"],
-      allowScriptAccess: "always",
-      forceHandCursor: true
-    });
-  }]).
-  directive('clipCopy', ['ngClip', function (ngClip) {
-    return {
-      scope: {
-        clipCopy: '&',
-        clipClick: '&'
-      },
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        // Create the clip object
-        var clip = new ZeroClipboard(element);
-        if (attrs.clipCopy == "") {
-          scope.clipCopy = function(scope) {
-            return element[0].previousElementSibling.innerText;
-          };
-        }
-        clip.on( 'load', function(client) {
-          var onDataRequested = function (client) {
-            client.setText(scope.$eval(scope.clipCopy));
-            if (angular.isDefined(attrs.clipClick)) {
-              scope.$apply(scope.clipClick);
-            }
-          };
-          client.on('dataRequested', onDataRequested);
-
-          scope.$on('$destroy', function() {
-            client.off('dataRequested', onDataRequested);
-            client.unclip(element);
-          });
-        });
-      }
-    };
-  }]);
+(function(window, define) {
+  function factory(angular, ZeroClipboard) {
+      'use strict';
+  
+      angular.module('ngClipboard', []).
+          provider('ngClip', function() {
+              var self = this;
+              this.path = '//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/1.3.2/ZeroClipboard.swf';
+              return {
+                  setPath: function(newPath) {
+                   self.path = newPath;
+                  },
+                  $get: function() {
+                      return {
+                          path: self.path
+                      };
+                  }
+              };
+          }).
+          run(['ngClip', function(ngClip) {
+              ZeroClipboard.config({
+                  moviePath: ngClip.path,
+                  trustedDomains: ["*"],
+                  allowScriptAccess: "always",
+                  forceHandCursor: true
+              });
+          }]).
+          directive('clipCopy', ['ngClip', function (ngClip) {
+              return {
+                  scope: {
+                      clipCopy: '&',
+                      clipClick: '&'
+                  },
+                  restrict: 'A',
+                  link: function (scope, element, attrs) {
+                      // Create the clip object
+                      var clip = new ZeroClipboard(element);
+                      clip.on( 'load', function(client) {
+                          var onDataRequested = function (client) {
+                              client.setText(scope.$eval(scope.clipCopy));
+                              if (angular.isDefined(attrs.clipClick)) {
+                                  scope.$apply(scope.clipClick);
+                              }
+                          };
+                          client.on('dataRequested', onDataRequested);
+  
+                          scope.$on('$destroy', function() {
+                              client.off('dataRequested', onDataRequested);
+                              client.unclip(element);
+                          });
+                      });
+                  }
+              };
+          }]);
+    
+    }
+    
+  if(('undefined' !== typeof define) && define.amd) {
+    define([ 'angular', 'zero-clipboard'], factory);
+  } else {
+    factory(window.angular, window.ZeroClipboard);
+  }
+  
+})(window, define);
